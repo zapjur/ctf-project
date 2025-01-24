b884b4ac8181ae0d0161bc692f506304ea52760d
Maciej <maciekjur123@gmail.com>
2025-01-24 22:48:24 +0100

    xddd

--- challenge3/challenge3-backend/src/main/java/com/example/challenge3_backend/Configuration/CorsConfig.java
+++ challenge3/challenge3-backend/src/main/java/com/example/challenge3_backend/Configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://twoj-frontend-url.com")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
